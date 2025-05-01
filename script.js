f7ca09bf74edd3175121012756b02fcea86047b7
Pratham Taikar <pravintaikar81@gmail.com>
2025-05-01 13:56:54 +0530

    Feature Update

--- script.js
+++ script.js
@@ -7,34 +7,42 @@ document.addEventListener("DOMContentLoaded", () => {
   let temperature = document.getElementById("temperature");
   let description = document.getElementById("description");
 
-  const API_KEY = <>Your Api Key</>;
+  const API_KEY = "ac60d0457ecf5529efb7a3134ab7c727";
 
+  //user location weather check
   document.getElementById("weatherbyLoc").addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition((position) => {
       const lat = position.coords.latitude;
       const lon = position.coords.longitude;
+      console.log(`Latitude: ${lat}
+        Longitude: ${lon}`);
       getWeather(lat, lon);
     });
     weatherInfo.classList.add("hidden");
     document.getElementById("weatherData").classList.remove("hidden");
   });
 
-  function getWeather(lat, lon) {
+  async function getWeather(lat, lon) {
     const API_KEY = "ac60d0457ecf5529efb7a3134ab7c727";
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
 
-    fetch(url)
+    await fetch(url)
       .then((res) => res.json())
       .then((data) => {
         document.getElementById("weatherData").innerHTML = `<center>
         <h2>${data.name}</h2>
+        <p>Country: ${data.sys.country}</p>
         <p>Temperature: ${data.main.temp}°C</p>
-        <p>Weather: ${data.weather[0].main}</p></center>
+        <p>Feels like: ${data.main.feels_like}°C</p>
+        <p>Wind Speed: ${data.wind.speed}°C</p>
+        <p>Weather: ${data.weather[0].main}</p>
+        </center>
       `;
       })
       .catch((err) => console.error(err));
   }
 
+  // customized weathwer check using the input as a city name.
   getWeatherBtn.addEventListener("click", async () => {
     const city = cityInput.value.trim();
     if (!city) return;
@@ -58,9 +66,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function displayWeatherData(data) {
     console.log(data);
-    const { name, main, weather } = data;
+    const { name, main, weather, sys, wind } = data;
     cityNameDisplay.innerHTML = name;
-    temperature.innerHTML = `,Temperature : ${main.temp}`;
+    temperature.innerHTML = `<p>
+    Country: ${sys.country}
+    <br>
+    Temperature : ${main.temp}
+    <br>
+    Feels like: ${main.feels_like}
+    <br>
+    Wind Speed: ${wind.speed}
+    </p>`;
     description.innerHTML = `Weather : ${weather[0].description}`;
 
     weatherInfo.classList.remove("hidden");
